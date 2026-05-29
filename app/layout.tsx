68105831fe681040ba09ba7458b60e7a59bf7df4
iwanha <iwanhadisetiawan07@gmail.com>
2026-05-29 16:31:26 +0700

    update: use async script src for Alibaba affiliate

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,13 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <script>
-          (function(){var a=document.createElement("script");
-          a.type="text/javascript";a.async=true;
-          a.src="https://ads.alicdn.com/publisher/loader.js?publisherId=1766765304";
-          var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(a,s);
-          })();
-        </script>
+        <script async src="https://ads.alicdn.com/publisher/loader.js?publisherId=1766765304"></script>
         {children}
       </body>
     </html>
